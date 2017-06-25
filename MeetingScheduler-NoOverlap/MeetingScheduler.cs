aa0c5a296951b2332bc7b9c5e40a73ce98cd7488
Abhimanyu Chugh <abhimanyuchugh@users.noreply.github.com>
2017-06-25 23:20:56 +0100

    Add separate project where the availabilities for a person never overlap (simpler case)

---
+++ MeetingScheduler-NoOverlap/MeetingScheduler.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MeetingScheduler
+{
+    internal class MeetingScheduler
+    {
+        public bool TryScheduleMeeting(List<PersonAvailability> personAvailabilities, HashSet<string> attendees,
+            DateTime fromDate, DateTime toDate, TimeSpan duration, out DateTime scheduledTime)
+        {
+            scheduledTime = DateTime.MinValue;
+            bool allAttendeesCovered;
+            var availabilityEvents = GetAvailabilityEvents(personAvailabilities, attendees, fromDate, toDate, duration, out allAttendeesCovered);
+            // if any attendee doesn't exist after the above filtering, terminate early
+            // since we won't be able to schedule meeting with everyone
+            if (!allAttendeesCovered)
+                return false;
+            // Sort by event time
+            availabilityEvents.Sort();
+            DateTime maxStartTime = fromDate;
+            var currAttendeesAvailable = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            foreach (var availabilityEvent in availabilityEvents)
+            {
+                var person = availabilityEvent.PersonAvailability.Person;
+                if (availabilityEvent.IsStart)
+                {
+                    if (maxStartTime < availabilityEvent.Timestamp)
+                        maxStartTime = availabilityEvent.Timestamp;
+                    // mark person as available
+                    currAttendeesAvailable.Add(person.Name);
+                    continue;
+                }
+                var isEndEventOutsideDateRange = availabilityEvent.Timestamp > toDate;
+                var endTime = isEndEventOutsideDateRange ? toDate : availabilityEvent.Timestamp;
+                // if all attendees are available and the current overlapping availability is
+                // the same length as (or more than) the duration, we've found a meeting time
+                if (currAttendeesAvailable.Count == attendees.Count && maxStartTime != null &&
+                    (endTime - maxStartTime) >= duration)
+                {
+                    scheduledTime = maxStartTime;
+                    return true;
+                }
+                // if the end event has reached the date limit, there's no need to continue
+                // since this person wouldn't be available anymore so we would never find a valid meeting time
+                if (isEndEventOutsideDateRange)
+                    return false;
+                // mark person as unavailable
+                currAttendeesAvailable.Remove(person.Name);
+            }
+            return false;
+        }
+
+        private List<PersonAvailabilityEvent> GetAvailabilityEvents(List<PersonAvailability> personAvailabilities, HashSet<string> attendees,
+            DateTime fromDate, DateTime toDate, TimeSpan duration, out bool allAttendeesCovered)
+        {
+            var attendeesNotCovered = new HashSet<string>(attendees);
+            var availabilityEvents = new List<PersonAvailabilityEvent>();
+            foreach (var personAvailability in personAvailabilities)
+            {
+                // ignore availabilities for people not in the attendees list or those with shorter durations than required
+                // or outside the given date range
+                if (!attendees.Contains(personAvailability.Person.Name) ||
+                    personAvailability.Duration < duration || personAvailability.From > toDate ||
+                    personAvailability.To < fromDate)
+                    continue;
+                attendeesNotCovered.Remove(personAvailability.Person.Name);
+                // create start and end events
+                availabilityEvents.Add(new PersonAvailabilityEvent(true, personAvailability.From, personAvailability));
+                availabilityEvents.Add(new PersonAvailabilityEvent(false, personAvailability.To, personAvailability));
+            }
+            allAttendeesCovered = attendeesNotCovered.Count == 0;
+            return availabilityEvents;
+        }
+    }
+}
---
+++ MeetingScheduler-NoOverlap/Person.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+
+namespace MeetingScheduler
+{
+    internal class Person
+    {
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+    }
+
+    internal class PersonAvailability
+    {
+        public PersonAvailability(Person person, DateTime from, DateTime to)
+        {
+            Person = person;
+            From = from;
+            To = to;
+            Duration = to - from;
+        }
+
+        public static bool TryParse(string data, out PersonAvailability personAvailability)
+        {
+            personAvailability = null;
+            if (string.IsNullOrEmpty(data)) return false;
+            var splits = data.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            if (splits.Length < 4) return false;
+            for (int i = 0; i < splits.Length; i++)
+            {
+                splits[i] = splits[i].Trim();
+            }
+            var person = new Person(splits[0]);
+            DateTime date;
+            TimeSpan from, to;
+            if (!DateTime.TryParseExact(splits[1], "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.AssumeLocal, out date) ||
+                !TimeSpan.TryParse(splits[2], out from) ||
+                !TimeSpan.TryParse(splits[3], out to) ||
+                to < from)
+                return false;
+            personAvailability = new PersonAvailability(person, date.Add(from), date.Add(to));
+            return true;
+        }
+
+        public static PersonAvailability Parse(string data)
+        {
+            PersonAvailability personAvailability;
+            if (!TryParse(data, out personAvailability))
+                throw new InvalidDataException(string.Format("Invalid row in availability data: {0}", data));
+            return personAvailability;
+        }
+
+        public Person Person { get; private set; }
+        public DateTime From { get; private set; }
+        public DateTime To { get; private set; }
+        public TimeSpan Duration { get; private set; }
+    }
+
+    internal class PersonAvailabilityEvent : IComparable<PersonAvailabilityEvent>
+    {
+        public PersonAvailabilityEvent(bool isStart, DateTime timestamp, PersonAvailability personAvailability)
+        {
+            IsStart = isStart;
+            Timestamp = timestamp;
+            PersonAvailability = personAvailability;
+        }
+
+        public bool IsStart { get; private set; }
+        public DateTime Timestamp { get; private set; }
+        public PersonAvailability PersonAvailability { get; private set; }
+
+        public int CompareTo(PersonAvailabilityEvent other)
+        {
+            if (other == null) return -1;
+            return Timestamp.CompareTo(other.Timestamp);
+        }
+    }
+
+}
